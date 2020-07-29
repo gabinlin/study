c4b1fb90c814725eaee73cc0d6d510fdb67a278d
gabin <18649660797@163.com>
2020-07-29 22:19:53 +0800

    勘误java8允许接口中有静态方法

--- src/top/gabin/patterns/observable/Tests.java
+++ src/top/gabin/patterns/observable/Tests.java
@@ -12,6 +12,7 @@ public class Tests {
         Event shopping = Event.SHOPPING;
         // 假设现在需要多种发送消息的方式，这边采取简单的硬编码，实际这种类型的模块功能也不会一直变
         // 对单个事件监听再分发不同的任务，理论上也算是一种观察者模式
+        // 日志写错了，是java8支持接口有静态方法
         List<MessageSend> messageSendList = List.of(msg -> {
             System.out.println("微信通知：" + msg);
             return true;
