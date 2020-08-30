eb1479764a3630c3da31ed7fd3a4b0d01c884862
gabin <18649660797@163.com>
2020-08-30 11:21:19 +0800

    单例补充

--- src/top/gabin/patterns/SimpleSingleton.java
+++ src/top/gabin/patterns/SimpleSingleton.java
@@ -3,8 +3,9 @@ package top.gabin.patterns;
 // 双重检查锁示例
 public class SimpleSingleton {
 
-    // 2、静态变量，一个类只有一个，多个实例也是共用一个;volatile告诉虚拟机和编译器
-    static volatile SimpleSingleton simpleSingleton;
+    // 2、静态变量，一个类只有一个，多个实例也是共用一个;
+    // volatile告诉虚拟机和编译器这里不要做指令重排
+    private static volatile SimpleSingleton simpleSingleton;
 
     // 1、私有化构造器，使得外部不能新建实例
     private SimpleSingleton() {
@@ -15,7 +16,9 @@ public class SimpleSingleton {
     public static SimpleSingleton getInstance() {
         if (simpleSingleton == null) {
             synchronized (SimpleSingleton.class) {
-                simpleSingleton = new SimpleSingleton();
+                if (simpleSingleton == null) { // 同步之后，需要再判断一次，避免其他线程修改
+                    simpleSingleton = new SimpleSingleton();
+                }
             }
         }
         return simpleSingleton;
