0feb07119aad641bf245a56ec68c6131374e9685
gabin <18649660797@163.com>
2021-07-13 11:07:10 +0800

    股票最大利润

---
+++ src/main/java/top/gabin/algorithm/MaxProfits.java
@@ -0,0 +1,26 @@
+package top.gabin.algorithm;
+
+public class MaxProfits {
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2)
+            return 0;
+        int length = prices.length;
+        //初始条件
+        int hold = -prices[0];//持有股票
+        int noHold = 0;//没持有股票
+        for (int i = 1; i < length; i++) {
+            //递推公式转化的
+            noHold = Math.max(noHold, hold + prices[i]);
+            hold = Math.max(hold, noHold - prices[i]);
+        }
+        //最后一天肯定是手里没有股票的时候利润才会最大，
+        //所以这里返回的是noHold
+        return noHold;
+    }
+
+}
